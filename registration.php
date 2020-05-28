e7acfce6a60cf971806f6aa97557a62e6c3625db
solwininfotech <info@solwininfotech.com>
2020-05-28 17:21:07 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin BookmarkManager Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_BookmarkManager
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_BookmarkManager',
+    __DIR__
+);
